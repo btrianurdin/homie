4ac0bb2307e40882266e3bc8111cf1e5cf76e9ae
Bagus TRIANURDIN <btstargroup@gmail.com>
2024-07-13 23:59:52 +0700

    feat(server/middleware): put input data on middleware

--- server/middleware/index.ts
+++ server/middleware/index.ts
@@ -1,3 +1,9 @@
-import { getServerSession, getToken } from "#auth";
+import { getServerSession } from "#auth";
 
-export default defineEventHandler(async (e) => {});
+export default defineEventHandler(async (e) => {
+  const session = await getServerSession(e);
+
+  if (session) {
+    e.context.user = session.user;
+  }
+});
